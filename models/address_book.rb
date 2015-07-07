8df905ba8135212c50fcd5e62e4afc000851ab5f
Rob Bankey <rob.bankey@gmail.com>
2015-07-07 16:03:54 -0400

    Assignment 21 complete

---
+++ models/address_book.rb
@@ -0,0 +1,29 @@
+require_relative "entry.rb"
+
+class AddressBook
+  attr_accessor :entries
+  
+  def initialize
+    @entries=[]
+  end
+  
+  def add_entry(name, phone, email)
+    index = 0
+    @entries.each do |entry|
+      if name < entry.name
+        break
+      end
+      index += 1       
+    end
+    @entries.insert(index, Entry.new(name, phone, email))
+  end  
+
+  def remove_entry(name)
+    index = 0
+    @entries.each do |entry|
+      if name == entry.name
+       @entries.delete_at(index)
+      end 
+    end  
+  end
+end  
